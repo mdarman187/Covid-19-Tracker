254363ab7318c80465621242dfb4cf1826950d5a
MD ARMAN <muhammadarman187@gmail.com>
2020-07-24 15:46:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,28 @@ def showData():
        active_patch = mpatches.Patch(color = 'blue', label = 'active')
        deaths_patch = mpatches.Patch(color = 'black', label = 'deaths')
        plt.legend(handles=[confirmed_patch,recovered_patch,active_patch,deaths_patch])
-       
-       
-             
+       for x in range(len(country_names)):
+             plt.bar(country_name[x], confirmed[x], color = 'green')
+             if recovered[x] > active[x]:
+                plt.bar(country_name[x], recovered[x], color = 'red')
+                plt.bar(country_name[x], active[x], color = 'blue')
+             else:
+                plt.bar(country_name[x], active[x], color = 'blue')
+                plt.bar(country_name[x], recovered[x], color = 'red')
+             plt.bar(country_name[x], deaths[x], color = 'black')
+        plt.title('Current Covid Cases')
+        plt.xlabel('Country name')
+        plt.ylabel('Cases(in millions)')
+        plt.show()
+     except Exception as e:
+        print("Enter correct name")
+        
+Label(root, text = "Enter countries names to\nto get COVID-19 details\n").pack()
+Label(root, text = "Enter country name:").pack()
+data = StringVar()
+data.set("Separate country names using Space")
+entry = Entry(root, textvariable = data, width = 50).pack()
+Button(root, text = "Provide Data", command = showData).pack()
+root.mainloop()
+        
     
